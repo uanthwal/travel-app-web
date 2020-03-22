7ec33da406358090880b30f01af04df392f1f7df
Upendra Parsad <upendra@Upendras-MacBook-Pro.local>
2020-03-22 13:19:03 -0300

    Home Page UI changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,138 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from '../_services';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "../_services";
 import { first } from "rxjs/operators";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
-  search_text: string = '';
-  constructor(private userService: UserService) { }
+  search_text: string = "";
+  searchResults = [];
+  constructor(private userService: UserService) {}
 
   ngOnInit() {
+    let tempobj = [
+      {
+        place_id: "WTN_NP",
+        place_name: "Waterton Lakes National Park",
+        province_id: "AL",
+        desc:
+          "The scenery is unreal and if you’re feeling adventurous, you can easily pop over the border to Montana – in a kayak (NBD). It’s the only park in the world to receive all three designations of UNESCO World Heritage Site, International Peace Park, and Biosphere Reserve.",
+        img_url:
+          "http://static.thousandwonders.net/Waterton.Lakes.National.Park.original.1407.jpg"
+      },
+      {
+        place_id: "WTN_NP",
+        place_name: "Waterton Lakes National Park",
+        province_id: "AL",
+        desc:
+          "The scenery is unreal and if you’re feeling adventurous, you can easily pop over the border to Montana – in a kayak (NBD). It’s the only park in the world to receive all three designations of UNESCO World Heritage Site, International Peace Park, and Biosphere Reserve.",
+        img_url:
+          "http://static.thousandwonders.net/Waterton.Lakes.National.Park.original.1407.jpg"
+      },
+      {
+        place_id: "WTN_NP",
+        place_name: "Waterton Lakes National Park",
+        province_id: "AL",
+        desc:
+          "The scenery is unreal and if you’re feeling adventurous, you can easily pop over the border to Montana – in a kayak (NBD). It’s the only park in the world to receive all three designations of UNESCO World Heritage Site, International Peace Park, and Biosphere Reserve.",
+        img_url:
+          "http://static.thousandwonders.net/Waterton.Lakes.National.Park.original.1407.jpg"
+      },
+      {
+        place_id: "WTN_NP",
+        place_name: "Waterton Lakes National Park",
+        province_id: "AL",
+        desc:
+          "The scenery is unreal and if you’re feeling adventurous, you can easily pop over the border to Montana – in a kayak (NBD). It’s the only park in the world to receive all three designations of UNESCO World Heritage Site, International Peace Park, and Biosphere Reserve.",
+        img_url:
+          "http://static.thousandwonders.net/Waterton.Lakes.National.Park.original.1407.jpg"
+      },
+      {
+        place_id: "WTN_NP",
+        place_name: "Waterton Lakes National Park",
+        province_id: "AL",
+        desc:
+          "The scenery is unreal and if you’re feeling adventurous, you can easily pop over the border to Montana – in a kayak (NBD). It’s the only park in the world to receive all three designations of UNESCO World Heritage Site, International Peace Park, and Biosphere Reserve.",
+        img_url:
+          "http://static.thousandwonders.net/Waterton.Lakes.National.Park.original.1407.jpg"
+      },
+      {
+        place_id: "WTN_NP",
+        place_name: "Waterton Lakes National Park",
+        province_id: "AL",
+        desc:
+          "The scenery is unreal and if you’re feeling adventurous, you can easily pop over the border to Montana – in a kayak (NBD). It’s the only park in the world to receive all three designations of UNESCO World Heritage Site, International Peace Park, and Biosphere Reserve.",
+        img_url:
+          "http://static.thousandwonders.net/Waterton.Lakes.National.Park.original.1407.jpg"
+      },
+      {
+        place_id: "WTN_NP",
+        place_name: "Waterton Lakes National Park",
+        province_id: "AL",
+        desc:
+          "The scenery is unreal and if you’re feeling adventurous, you can easily pop over the border to Montana – in a kayak (NBD). It’s the only park in the world to receive all three designations of UNESCO World Heritage Site, International Peace Park, and Biosphere Reserve.",
+        img_url:
+          "http://static.thousandwonders.net/Waterton.Lakes.National.Park.original.1407.jpg"
+      },
+      {
+        place_id: "WTN_NP",
+        place_name: "Waterton Lakes National Park",
+        province_id: "AL",
+        desc:
+          "The scenery is unreal and if you’re feeling adventurous, you can easily pop over the border to Montana – in a kayak (NBD). It’s the only park in the world to receive all three designations of UNESCO World Heritage Site, International Peace Park, and Biosphere Reserve.",
+        img_url:
+          "http://static.thousandwonders.net/Waterton.Lakes.National.Park.original.1407.jpg"
+      },
+      {
+        place_id: "WTN_NP",
+        place_name: "Waterton Lakes National Park",
+        province_id: "AL",
+        desc:
+          "The scenery is unreal and if you’re feeling adventurous, you can easily pop over the border to Montana – in a kayak (NBD). It’s the only park in the world to receive all three designations of UNESCO World Heritage Site, International Peace Park, and Biosphere Reserve.",
+        img_url:
+          "http://static.thousandwonders.net/Waterton.Lakes.National.Park.original.1407.jpg"
+      },
+      {
+        place_id: "WTN_NP",
+        place_name: "Waterton Lakes National Park",
+        province_id: "AL",
+        desc:
+          "The scenery is unreal and if you’re feeling adventurous, you can easily pop over the border to Montana – in a kayak (NBD). It’s the only park in the world to receive all three designations of UNESCO World Heritage Site, International Peace Park, and Biosphere Reserve.",
+        img_url:
+          "http://static.thousandwonders.net/Waterton.Lakes.National.Park.original.1407.jpg"
+      },
+      {
+        place_id: "WTN_NP",
+        place_name: "Waterton Lakes National Park",
+        province_id: "AL",
+        desc:
+          "The scenery is unreal and if you’re feeling adventurous, you can easily pop over the border to Montana – in a kayak (NBD). It’s the only park in the world to receive all three designations of UNESCO World Heritage Site, International Peace Park, and Biosphere Reserve.",
+        img_url:
+          "http://static.thousandwonders.net/Waterton.Lakes.National.Park.original.1407.jpg"
+      },
+      {
+        place_id: "WTN_NP",
+        place_name: "Waterton Lakes National Park",
+        province_id: "AL",
+        desc:
+          "The scenery is unreal and if you’re feeling adventurous, you can easily pop over the border to Montana – in a kayak (NBD). It’s the only park in the world to receive all three designations of UNESCO World Heritage Site, International Peace Park, and Biosphere Reserve.",
+        img_url:
+          "http://static.thousandwonders.net/Waterton.Lakes.National.Park.original.1407.jpg"
+      },
+      {
+        place_id: "WTN_NP",
+        place_name: "Waterton Lakes National Park",
+        province_id: "AL",
+        desc:
+          "The scenery is unreal and if you’re feeling adventurous, you can easily pop over the border to Montana – in a kayak (NBD). It’s the only park in the world to receive all three designations of UNESCO World Heritage Site, International Peace Park, and Biosphere Reserve.",
+        img_url:
+          "http://static.thousandwonders.net/Waterton.Lakes.National.Park.original.1407.jpg"
+      }
+    ];
+    this.searchResults = tempobj;
   }
 
   onClickSearch() {
@@ -19,16 +140,18 @@ export class HomeComponent implements OnInit {
     if (this.search_text == "") {
       return;
     }
+    this.searchResults = [];
     this.userService
-    .search({search_text:this.search_text, session_id:JSON.parse(localStorage.getItem('session_id'))})
-    .pipe(first())
-    .subscribe(
-      data => {
-        
-      },
-      error => {
-       
-      }
-    );
-}
+      .search({
+        search_text: this.search_text,
+        session_id: JSON.parse(localStorage.getItem("session_id"))
+      })
+      .pipe(first())
+      .subscribe(
+        responseData => {
+          this.searchResults = responseData["data"];
+        },
+        error => {}
+      );
+  }
 }
